06032ad3bbe778608e7bb6ee22535f10548939ff
sarfraj0304 <sarfrajahmadraza@gmail.com>
2023-03-03 20:10:15 +0530

    Vid Done!

--- client/src/pages/homePage/Home.tsx
+++ client/src/pages/homePage/Home.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import HomeVid from "./Home.mp4";
+import { Box } from "@chakra-ui/layout";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Box width={"100%"} height={"100vh"}>
+        <video
+          src={HomeVid}
+          muted
+          autoPlay
+          loop
+          style={{
+            position: "relative",
+            width: "100%",
+            height: "100%",
+            zIndex: -1,
+            objectFit: "cover",
+          }}
+        ></video>
+      </Box>
+    </>
+  );
+};
 
-export default Home
+export default Home;
